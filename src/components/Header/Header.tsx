8fef1e0a963f9914706226db7b2c274ee10be5a9
EkaterinaYurchyk2022 <ekaterina.yurchyk.s@gmail.com>
2022-03-10 16:47:40 +0300

    modified css files

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.module.css';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img
+                src='https://static.independent.co.uk/2021/12/07/10/PRI213893584.jpg?quality=75&width=990&auto=webp&crop=982:726,smart'/>
+        </header>
+    );
+};
+
+export default Header;
---
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import s from './MyPosts.module.css';
+import Post from "./Post/Post";
+
+
+type MyPostPropsType = {
+    message: string
+    likesCounts: number
+}
+
+const MyPosts = () => {
+    return (
+        <div>
+            My posts
+            <div>
+                <textarea></textarea>
+                <button>Add post</button>
+
+            </div>
+            <div className={s.posts}>
+                <Post message={'Hi, how are you?'} likesCounts={15}/>
+                <Post message={"It's my first post"} likesCounts={25}/>
+            </div>
+        </div>
+
+    );
+};
+
+export default MyPosts;
