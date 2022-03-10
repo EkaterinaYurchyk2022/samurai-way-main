eb051d29b86ffd3066dff97b1f88b54270166b03
EkaterinaYurchyk2022 <ekaterina.yurchyk.s@gmail.com>
2022-03-10 23:39:38 +0300

    Change README.md

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,38 +1,49 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom';
 import s from './Dialogs.module.css'
 
-const Dialogs = () => {
+type DialogItemPropsType = {
+    name: string
+    id: number
+}
+const DialogItem = (props: DialogItemPropsType) => {
+    let path = "/dialogs/" + props.id;
+    return <div className={s.dialog + ' ' + s.active}>
+        <NavLink to={path}>{props.name}</NavLink>
+    </div>
+}
+type MessagePropsType = {
+    message: string
+}
+
+const Message = (props: MessagePropsType) => {
+    return <div className={s.dialog}>{props.message}</div>
+}
+
+type DialogsPropsType = {
+    name: DialogItemPropsType
+    id: DialogItemPropsType
+}
+
+const Dialogs = (props: DialogsPropsType) => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <div className={s.dialog + ' ' + s.active}>
-                    Ekaterina
-                </div>
-                <div className={s.dialog}>
-                    Andrey
-                </div>
-                <div className={s.dialog}>
-                    Anna
-                </div>
-                <div className={s.dialog}>
-                    Ivan
-                </div>
-                <div className={s.dialog}>
-                    Sasha
-                </div>
-                <div className={s.dialog}>
-                    Mariya
-                </div>
-                <div className={s.dialog}>
-                    Nastya
-                </div>
+                <DialogItem name="Ekaterina" id={1}/>
+                <DialogItem name="Andrey" id={2}/>
+                <DialogItem name="Anna" id={3}/>
+                <DialogItem name="Ivan" id={4}/>
+                <DialogItem name="Sasha" id={5}/>
+                <DialogItem name="Mariya" id={6}/>
+                <DialogItem name="Anastasiya" id={7}/>
+
             </div>
             <div className={s.messages}>
-<div className={s.message}>Hi</div>
-<div className={s.message}>How is your it-kamasutra?</div>
-<div className={s.message}>Yo</div>
-
-
+                <Message message="Hi"/>
+                <Message message="How is your it-kamasutra?"/>
+                <Message message="Yo"/>
+                <Message message="Yo"/>
+                <Message message="Yo"/>
             </div>
         </div>
     );
--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -3,25 +3,26 @@ import s from './MyPosts.module.css';
 import Post from "./Post/Post";
 
 
-
 type MyPostPropsType = {
     message: string
     likesCounts: number
 }
-const MyPosts: React.FC<MyPostPropsType> = (props) => {
+const MyPosts = (props) => {
     return (
         <div>
-            My posts
-            <div>
-                <textarea></textarea>
-                <button>Add post</button>
-
+            <h3> My posts </h3>
+            <div className={s.postsBlock}>
+                <div>
+                    <textarea></textarea>
+                </div>
+                <div>
+                    <button>Add post</button>
+                </div>
             </div>
             <div className={s.posts}>
 
                 <Post message='Hi, how are you?' likesCounts={15}/>
                 <Post message="It's my first post" likesCounts={25}/>
-
 
 
             </div>
